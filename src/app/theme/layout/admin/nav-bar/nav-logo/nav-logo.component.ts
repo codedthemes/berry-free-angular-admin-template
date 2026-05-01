fc360b8d23e6801e83606782279b395363c80058
ct-krisha <krisha.jetani@codedthemes.com>
2026-05-01 12:14:42 +0530

    remove extra css and add a tag

--- src/app/theme/layout/admin/nav-bar/nav-logo/nav-logo.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-logo/nav-logo.component.ts
@@ -1,13 +1,13 @@
 // Angular import
 import { Component, output, inject, model } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 // project import
 import { SHARED_IMPORTS } from 'src/app/theme/shared/shared.module';
 
 @Component({
   selector: 'app-nav-logo',
-  imports: [...SHARED_IMPORTS],
+  imports: [...SHARED_IMPORTS, RouterLink],
   templateUrl: './nav-logo.component.html',
   styleUrl: './nav-logo.component.scss'
 })
